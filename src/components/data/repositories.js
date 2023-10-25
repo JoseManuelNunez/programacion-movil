1339127b0881f3e2de7a8d04c6b2cb51b2102e21
Jose Nuñez <josemanuelnunezreyes@gmail.com>
2023-10-25 15:50:14 -0400

    add data of repositories

---
+++ src/components/data/repositories.js
@@ -0,0 +1,75 @@
+export default[ 
+    {
+    id: 'midudev.midu.dev',
+    fullName: 'midudev/midu.dev',
+    description: 'Blog de noticias sobre desarrollo',
+    Language: 'HTML',
+    forksCount: 3123,
+    stargazersCount: 52869,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4' 
+    },
+
+    {
+    id: 'jaredpalmer. formik', fullName: 'jaredpalmer/formik',
+    description: 'Build forms in React, without the tears',
+    Language: 'TypeScript',
+    forksCount: 1589,
+    stargazersCount: 21553, ratingAverage: 88,
+    reviewCount: 4,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4'  
+    },
+    {
+    id: 'rails.rails',
+    fullName:'rails/rails',
+    description: 'Ruby on Rails',
+    Language: 'Ruby',
+    forksCount: 18349, stargazersCount: 45377,
+    ratingAverage: 100, 
+    reviewCount: 2, 
+    ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4'     },
+    { 
+    id: 'django.django',
+    fullName: 'django/django',
+    description: 'The Web framework for perfectionists with deadlines.',
+    Language: 'Python',
+    forksCount: 21015,
+    stargazersCount: 48495,
+    ratingAverage: 73,
+    reviewCount: 5,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4'
+},
+{
+    id: 'reduxjs.redux',
+    fullName: 'reduxjs/redux',
+    description: 'Predictable state container for JavaScript apps',
+    Language: 'TypeScript',
+    forksCount: 13902,
+    stargazersCount: 52869,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4'
+},
+{
+    id: 'Laravel.Laravel',
+    fullName: 'Laravel',
+    description: 'Laravel is a free, open-source PHP web framework used for web application development.',
+    Language: 'PHP',
+    forksCount: 2350,
+    stargazersCount: 17800,
+    ratingAverage: 4.8,
+    reviewCount: 420,
+    ownerAvatarUrl: 'https://logospng.org/download/laravel/logo-laravel-icon-1024.png'
+},
+{
+    fullName: 'Vue.js',
+    description: 'Vue.js is a progressive JavaScript framework for building user interfaces.',
+    Language: 'JavaScript',
+    forksCount: 3200,
+    stargazersCount: 21500,
+    ratingAverage: 4.9,
+    reviewCount: 600,
+    ownerAvatarUrl: 'https://mochikit.com/wp-content/uploads/2019/01/vue-js.jpg'
+}
+]
